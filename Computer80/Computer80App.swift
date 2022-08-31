cc79442ef331dbe79a6a242e3d34fa416ac0f19b
jode68 <xumepao@yahoo.it>
2022-08-31 22:51:44 +0200

    Initial Commit

---
+++ Computer80/Computer80App.swift
@@ -0,0 +1,17 @@
+//
+//  Computer80App.swift
+//  Computer80
+//
+//  Created by Paolo Xumerle on 31/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Computer80App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Computer80/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Computer80
+//
+//  Created by Paolo Xumerle on 31/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
